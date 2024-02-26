f77614fbbc09a0ba2ded874b39f3d3f3fce9c69c
Joel Troncoso <joeltroncoso2002@gmail.com>
2024-02-26 20:11:57 -0300

    commit modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,63 @@
+$(document).ready(function(){
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
 //Ocultar card
-$(document).ready(function(){
+
    
     $("#clc").click(function(){
      
       $("#ocultado").toggle();
     });
-  });
+ 
 
-  $(document).ready(function(){
+ 
    
     $("#clc2").click(function(){
      
       $("#ocultado2").toggle();
     });
-  });
+
   
-  $(document).ready(function(){
-   
+
     $("#clc3").click(function(){
      
       $("#ocultado3").toggle();
     });
-  });
-  
-  $(document).ready(function(){
+
+
    
     $("#clc4").click(function(){
      
       $("#ocultado4").toggle();
     });
-  });
+
   
 
   //Boton para mostrar cards ocultas
 
-  $(document).ready(function(){
+
     $("#mostrarBoton").click(function(){
       $("#ocultado").toggle();
     });
-  });
-  $(document).ready(function(){
+
+
     $("#mostrarBoton").click(function(){
       $("#ocultado2").toggle();
     });
-  });
-  $(document).ready(function(){
+
+  
     $("#mostrarBoton").click(function(){
       $("#ocultado3").toggle();
     });
-  });
+ 
 
-  $(document).ready(function(){
+
     $("#mostrarBoton").click(function(){
       $("#ocultado4").toggle();
     });
-  });
+
 
 
 //Modal
@@ -66,3 +65,18 @@ $(document).ready(function(){
     $('#myInput').trigger('focus')
   })
   
+       function verificarCampos() {
+        var campo1Value = document.getElementById('nombre').value;
+        var campo2Value = document.getElementById('asunto').value;
+        var campo3Value = document.getElementById('mensaje').value;
+        if (campo1Value.trim() !== '' && campo2Value.trim() !== ''&& campo3Value.trim() !== '') {
+          document.getElementById('btnEnviar').disabled = false;
+        } else {
+          document.getElementById('btnEnviar').disabled = true;
+        }
+      }
+      document.getElementById('nombre').addEventListener('input', verificarCampos);
+      document.getElementById('asunto').addEventListener('input', verificarCampos);
+      document.getElementById('mensaje').addEventListener('input', verificarCampos);
+
+})
